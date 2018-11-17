e5fc842614e275735c533cc58e2965dae3e94ba3
kazukinakamuraKN <kajirikajiri@gmail.com>
2018-11-17 21:22:49 +0900

    add circle ci and change component

--- src/router/index.js
+++ src/router/index.js
@@ -10,6 +10,11 @@ export default new Router({
       path: '/',
       name: 'HelloWorld',
       component: HelloWorld
+    },
+    {
+    	path: '/hello',
+    	name: 'HelloWorld',
+    	component: HelloWorld
     }
   ]
 })
